02ba26036f81ca0aa7674a09fb89e8f0b65c9318
root <root@timmi-imnu>
2022-10-21 00:18:46 +0200

    invoice server via systemd

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Deployment
 
-* configure `/etc/timmi` (or use sops)
+* configure `/etc/timmi`and `/etc/timmi-invoice` (or use sops)
+
+## Updates
+
+```bash
+cd /etc/nixos
+nix flake update
+nixos-rebuild switch
+```
--- modules/timmi/systemd.nix
+++ modules/timmi/systemd.nix
@@ -1,19 +1,37 @@
 {config, pkgs, ...}:
 {
-  ## TODO: separate services for client/server + add service for invoice
+  ## TODO: separate services for client/server
 
   systemd.services = {
+
     "timmi-client" = {
       path = with pkgs; [ nix git ];
       script = ''
         nix run
       '';
       serviceConfig = rec {
+        Type = "forking";
+        TimeoutStartSec = 600;
         WorkingDirectory = "${config.users.users."timmi-client".home}/pro";
         User = "timmi-client";
         Group = config.users.users."timmi-client".group;
       };
       wantedBy = [ "multi-user.target" ];
     };
+
+    "timmi-invoice" = {
+      path = with pkgs; [ nix git ];
+      script = ''
+        nix run
+      '';
+      serviceConfig = rec {
+        Type = "simple";
+        WorkingDirectory = "${config.users.users."timmi-invoice".home}/invoice";
+        User = "timmi-invoice";
+        Group = config.users.users."timmi-invoice".group;
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+
   };
 }
